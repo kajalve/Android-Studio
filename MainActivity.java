0a1a389bc9f02744e1d35e9598fbce37cc9b9dce
kajalve <51096087+kajalve@users.noreply.github.com>
2019-06-07 01:28:12 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,79 +1,45 @@
-package com.example.listview;
+package com.example.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-Spinner list;
-    String data[]={"kajal","Priyanshi","Agrima","Ananya","Aprajita"};
-
+    EditText fname, lname;
+    Button submit;
+    TextView fullname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list=(Spinner) findViewById(R.id.listview);
-
-        ArrayAdapter adapter=new ArrayAdapter(MainActivity.this,
-                android.R.layout.simple_list_item_1,data);
-        list.setAdapter(adapter);
-        list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        fname = (EditText) findViewById(R.id.fname);
+        lname = (EditText) findViewById(R.id.lname);
+        submit = (Button) findViewById(R.id.submit);
+        fullname = (TextView) findViewById(R.id.fullname);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // TextView txt=(TextView)view;
-                //Toast.makeText(MainActivity.this, txt.getText(),Toast.LENGTH_SHORT).show();
-
-                switch (position)
-            {
-               case 0:
-                    Toast.makeText(MainActivity.this, "First",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-              case 1:
-                    Toast.makeText(MainActivity.this, "Second",
-                            Toast.LENGTH_SHORT).show();
-                  break;
-               case 2:
-                    Toast.makeText(MainActivity.this, "Third",
-                           Toast.LENGTH_SHORT).show();
-                   break;
-            }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//               // TextView txt=(TextView)view;
-//                //Toast.makeText(MainActivity.this, txt.getText(),Toast.LENGTH_SHORT).show();
-//            switch (position)
-//            {
-//                case 0:
-//                    Toast.makeText(MainActivity.this, "First",
-//                            Toast.LENGTH_SHORT).show();
-//                    break;
-//                case 1:
-//                    Toast.makeText(MainActivity.this, "Second",
-//                            Toast.LENGTH_SHORT).show();
-//                    break;
-//                case 2:
-//                    Toast.makeText(MainActivity.this, "Third",
-//                            Toast.LENGTH_SHORT).show();
-//                    break;
-//            }
-//            }
-//        });
-    }
+            public void onClick(View v) {
+                String fname1 = fname.getText().toString();
+                String lname1 = lname.getText().toString();
+                if(TextUtils.isEmpty(fname1))
+                {
+                    fname.setError("Enter First name");
+                }
+                else if(TextUtils.isEmpty(lname1))
+                    {
+                    lname.setError("Enter Last Name");
+                    }
+                    else
+                {
+                fullname.setText(fname1 + " " + lname1);
+                }
+        }
+    });
+}
 }
